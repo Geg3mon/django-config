9d2d1911fd796c9b43f013505da82bc07156021a
Yevhenii Hrybanov <>
2022-10-07 14:25:07 +0300

    -help

---
+++ config/choices.py
@@ -0,0 +1,20 @@
+CURRENCY = [
+    ('USD', 'US Dollar'),
+    ('EUR', 'Euro'),
+    ('UAH', 'Ukrainian Hryvnia'),
+    ('PLN', 'Zloty'),
+    ('KZT', 'Tenge'),
+    ('RUB', 'Russian Ruble'),
+    ('BYN', 'Belarussian Ruble'),
+    ('BGN', 'Bulgarian Lev'),
+    ('TJS', 'Somoni'),
+    ('TRY', 'Turkish Lira'),
+    ('EUR', 'Euro'),
+
+]
+LANGUAGE = [
+    ('en', 'English'),
+    ('ua', 'Ukrainian')
+    ('ru', 'Russian'),
+
+]
